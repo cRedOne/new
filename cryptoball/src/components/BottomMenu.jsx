eb844813cafe365ddedb24a22b6590508f941746
cRedOne <lidao2005@gmail.com>
2024-11-28 11:48:54 +0200

    Initial commit excluding hidden and temporary files

---
+++ cryptoball/src/components/BottomMenu.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const BottomMenu = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="bottom-menu">
+            <div className="button" onClick={() => navigate('/')}>Главный экран</div>
+            <div className="button" onClick={() => navigate('/page2')}>Страница 2</div>
+            <div className="button" onClick={() => navigate('/page3')}>Страница 3</div>
+        </div>
+    );
+};
+
+export default BottomMenu;
---
+++ cryptoball/src/pages/Page2.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+const Page2 = () => {
+    return (
+        <div>
+            <h1>Страница 2</h1>
+            {/* Контент страницы 2 */}
+        </div>
+    );
+};
+
+export default Page2;
