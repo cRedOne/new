2516df2c9800c13ad6984de68673dbb3c41b60fc
cRedOne <lidao2005@gmail.com>
2024-11-28 12:56:34 +0200

    Build project with Vite

--- cryptoball/vite.config.js
+++ cryptoball/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    base: '/new/',  // Убедитесь, что base путь правильный
     server: {
         port: 49942,
+    },
+    build: {
+        outDir: 'dist'
     }
-})
+});
